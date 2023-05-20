d6c3ed0da249ee3ba69819c928421c65febf93cb
Francisop <francisohis@gmail.com>
2023-05-20 12:45:05 +0100

    removed mutable

--- userManagement/views.py
+++ userManagement/views.py
@@ -132,7 +132,7 @@ def signin(request):
     else:
         data = {'token': user_token, 'role': user.role, 'username': user.matric,
                 "first_name": user.first_name, "last_name": user.last_name, "gender": user.gender,
-                "other_name": user.other_name, "class": user.school_class, "sesssion": user.session, "dp": user.dp.url,
+                "other_name": user.other_name, "class": user.school_class, "sesssion": user.session,
                 "active": user.is_active}
         return Response(data=data, status=status.HTTP_200_OK)
 
